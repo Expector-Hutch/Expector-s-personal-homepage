cd8688e4aa4362d1628eb1774adb3ca432aeb716
Expector <expector-hutch@outlook.com>
2024-11-06 20:06:09 +0800

    尝试解决cdn问题

--- vite.config.ts
+++ vite.config.ts
@@ -25,17 +25,17 @@ export default defineConfig({
                 {
                     name: 'aplayer',
                     var: 'aplayer',
-                    path: 'https://cdn.jsdelivr.net/npm/aplayer@1.10.1/dist/APlayer.min.js',
-                    css: 'https://cdn.jsdelivr.net/npm/aplayer@1.10.1/dist/APlayer.min.css'
+                    path: '/cdn/aplayer@1.10.1/dist/APlayer.min.js',
+                    css: '/cdn/aplayer@1.10.1/dist/APlayer.min.css'
                 },
                 {
                     name: 'tippy.js',
                     var: 'tippy.js',
-                    path: 'https://cdn.jsdelivr.net/npm/tippy.js@6.3.7/dist/tippy.esm.min.js',
+                    path: '/cdn/tippy.js@6.3.7/dist/tippy.esm.min.js',
                     css: [
-                        'https://cdn.jsdelivr.net/npm/tippy.js@6.3.7/dist/tippy.css',
-                        'https://cdn.jsdelivr.net/npm/tippy.js@6.3.7/dist/backdrop.css',
-                        'https://cdn.jsdelivr.net/npm/tippy.js@6.3.7/animations/scale.css',
+                        '/cdn/tippy.js@6.3.7/dist/tippy.css',
+                        '/cdn/tippy.js@6.3.7/dist/backdrop.css',
+                        '/cdn/tippy.js@6.3.7/animations/scale.css',
                     ]
                 }
             ]
@@ -59,6 +59,11 @@ export default defineConfig({
                 target: 'https://cdn.simpleicons.org',
                 changeOrigin: true,
                 rewrite: (path) => path.replace(/^\/icons/, ''),
+            },
+            '/cdn': {
+                target: 'https://cdn.jsdelivr.net/npm',
+                changeOrigin: true,
+                rewrite: (path) => path.replace(/^\/cdn/, ''),
             }
         },
     }
