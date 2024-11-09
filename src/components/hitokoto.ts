44dbf82c4cd031c66ad8f56c8410912661bac66f
Expector <expector-hutch@outlook.com>
2024-11-09 13:21:20 +0800

    更新 hitokoto.ts

--- src/components/hitokoto.ts
+++ src/components/hitokoto.ts
@@ -8,29 +8,26 @@ export class HitokotoElement extends HTMLElement {
 
     async getHitokoto(): Promise<string> {
         try {
-            const response = await fetch('https://v1.hitokoto.cn');
+            const response = await fetch('https://tenapi.cn/v2/yiyan');
             if (!response.ok) {
                 throw new Error(`HTTP error! status: ${response.status}`);
             }
-            console.log("v1.hitokoto.cn");
-            const { hitokoto: hitokotoText } = await response.json();
-            return hitokotoText;
+            return await response.text();
         } catch (error) {
             try {
-                const response = await fetch('https://tenapi.cn/v2/yiyan');
+                const response = await fetch('https://uapis.cn/api/say');
                 if (!response.ok) {
                     throw new Error(`HTTP error! status: ${response.status}`);
                 }
-                console.log("tenapi.cn");
                 return await response.text();
             } catch (error) {
                 try {
-                    const response = await fetch('https://uapis.cn/api/say');
+                    const response = await fetch('https://v1.hitokoto.cn');
                     if (!response.ok) {
                         throw new Error(`HTTP error! status: ${response.status}`);
                     }
-                    console.log("uapis.cn");
-                    return await response.text();
+                    const { hitokoto: hitokotoText } = await response.json();
+                    return hitokotoText;
                 } catch (error) {
                     throw error;
                 }
