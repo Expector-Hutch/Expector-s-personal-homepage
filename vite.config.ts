517dffc43825c89301c51f6d2b440cb175768cb3
Expector <expector-hutch@outlook.com>
2024-11-13 19:45:24 +0800

    更新 vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,26 +3,27 @@ import { defineConfig } from 'vite';
 import Icons from 'unplugin-icons/vite';
 
 export default defineConfig({
+    base: '/Expector-s-personal-homepage/',
     plugins: [
         Icons({
             autoInstall: true,
             compiler: 'raw',
-        })
+        }),
     ],
     build: {
         rollupOptions: {
             input: {
                 index: './index.html',
-                404: './404.html'
-            }
-        }
+                404: './404.html',
+            },
+        },
     },
     css: {
         preprocessorOptions: {
             scss: {
-                api: 'modern'
-            }
-        }
+                api: 'modern',
+            },
+        },
     },
     server: {
         proxy: {
@@ -30,7 +31,7 @@ export default defineConfig({
                 target: 'http://music.163.com',
                 changeOrigin: true,
                 rewrite: (path) => path.replace(/^\/music/, ''),
-            }
+            },
         },
-    }
+    },
 });
