4deff9ada723b3c098b17c7ca288e72222254b0c
Expector <105506585+Expector-Hutch@users.noreply.github.com>
2026-04-08 22:52:29 +0800

    优化light_btn逻辑

--- src/components/light_btn.tsx
+++ src/components/light_btn.tsx
@@ -1,26 +1,38 @@
 import style from "./light_btn.module.scss";
 
-import { generateID } from "../utils/generateID";
-
 export class LightBtnElement extends HTMLElement {
     constructor() {
         super();
-        const id = generateID();
         const color = this.getAttribute("color");
         const text = this.innerHTML;
+        const id = (color as string)
+            .split("")
+            .map(c =>
+                c
+                    .charCodeAt(0)
+                    .toString(16)
+                    .padStart(2, "0")
+                    .split("")
+                    .map(h => String.fromCharCode(65 + parseInt(h, 16)))
+                    .join("")
+            )
+            .join("");
         this.replaceChildren(
             <div class={`${style["light-btn"]} light-btn light-btn-${id}`}>
                 <a href={this.getAttribute("href")} target="_blank">
                     {text}
                 </a>
                 <style>
-                    {`.light-btn-${id}::before, .light-btn-${id}::after {
-                    background-color: ${color};
-                    box-shadow: 0 0 5px ${color},
-                        0 0 15px ${color},
-                        0 0 30px ${color},
-                        0 0 60px ${color};
-                }`}
+                    {
+                        /* css */ `
+                        .light-btn-${id}::before, .light-btn-${id}::after {
+                            background-color: ${color};
+                            box-shadow: 0 0 5px ${color},
+                                0 0 15px ${color},
+                                0 0 30px ${color},
+                                0 0 60px ${color};
+                        }`
+                    }
                 </style>
             </div>
         );
